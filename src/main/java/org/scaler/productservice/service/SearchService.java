5d9813380b92a541c4c8e14ece2010e00e3c7577
upendra2610 <upendra.singh2610@gmail.com>
2024-05-08 10:20:07 +0530

    implmented sorting via pageable

--- src/main/java/org/scaler/productservice/service/SearchService.java
+++ src/main/java/org/scaler/productservice/service/SearchService.java
@@ -5,6 +5,7 @@ import org.scaler.productservice.repository.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
@@ -16,7 +17,10 @@ public class SearchService {
         this.productRepository = productRepository;
     }
     public Page<Product> search(String query, int pageNumber, int pageSize){
-        Pageable pageable = PageRequest.of(pageNumber,pageSize);
+        Sort sort = Sort.by("title").descending()
+                .and(Sort.by("price").ascending());
+
+        Pageable pageable = PageRequest.of(pageNumber,pageSize, sort);
         return productRepository.findByTitleContaining(query,pageable);
 
     }
