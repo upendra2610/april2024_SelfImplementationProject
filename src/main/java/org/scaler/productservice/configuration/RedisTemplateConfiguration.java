6cfdf41bda9a26ac7a54baecb3dd30004de1b28c
upendra2610 <upendra.singh2610@gmail.com>
2024-05-16 17:37:39 +0530

    added Redis Notes(for interview) in RedisTemplateConfiguraton class

--- src/main/java/org/scaler/productservice/configuration/RedisTemplateConfiguration.java
+++ src/main/java/org/scaler/productservice/configuration/RedisTemplateConfiguration.java
@@ -18,5 +18,23 @@ public class RedisTemplateConfiguration {
         return redisTemplate;
     }
 }
+//NOTES(Regarding redis cache)
+//1. RedisTemplate<String, Object> is storing data in cache in String as Key and Object -
+//    as value manner;
 
+//2. RedisConnectionFactory contains details around how to connect to redis instance -
+//    (like all url etc. inside it)
+
+//3. at the actual value of url and port we need to provide that value put inside -
+//    properties file(check tha file)
+
+//4. all the things regarding how connection needs to happen or when need to happen -
+//    all taking care by my spring data and has inbuilt support fo this.
+
+//5. When we deploy, aws provides "ElasticCache" for redis like RDS.
+
+//6. when we save inside redis then this call happen in network and redis not understand objects -
+//   so it serialize(converting into set of bytes) and send data over network and when we get -
+//   data from redis then it deserialize into object and for this we need to implement Serializable -
+//   interface to allow for serializing in particular class.
 
